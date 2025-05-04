5d613dff706ab322446b1e41c5d9752d7a13b118
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-04 18:26:40 +0300

    working with annotations

---
+++ features.ts
@@ -0,0 +1,12 @@
+
+
+let num: number = 5;
+let speed: string = 'fast'
+let hasName: boolean = true
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined
+
+let time: Date = new Date()
+
+console.log(time)
--- index.ts
+++ index.ts
@@ -1,25 +1,16 @@
 import axios from 'axios';
-
 type Todo = {
   userId: number;
   id: number;
   title: string;
   completed: boolean;
 };
-
 const URL = 'https://jsonplaceholder.typicode.com/todos/3';
-
-
 async function fetchData(url: string): Promise<Todo> {
-
   const res = await axios.get(url)
   const data = res.data as Todo
-
-
   return data
-
 }
-
 const data = fetchData(URL).then(dat => console.log(dat))
 
 
